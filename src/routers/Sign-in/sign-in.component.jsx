2d25ced718f09d7f09a9a443024d98efc133ffcb
Florian Ipate <florian.ipate@gmail.com>
2024-07-19 14:50:45 +0100

    make it better

--- src/routers/Sign-in/sign-in.component.jsx
+++ src/routers/Sign-in/sign-in.component.jsx
@@ -1,7 +1,13 @@
+import { signInWithGooglePopup } from "../../utils/firebase/utils";
 const SignIn = ()=>{
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
     return(
         <div>
             <h1>Sign In Page</h1>
+            <button onClick={logGoogleUser}>Login with Google</button>
         </div>
     );
 };
